e3602cb9835a0f4a8699ce6c8ba063e87c3426ef
h_li <h_li@dreamats.co.jp>
2014-02-04 14:06:09 +0800

    first commitfirst commit

--- app.js
+++ app.js
@@ -5,44 +5,21 @@ app.all('/token.json', function(req, res){
 
 
 
+
+
   var signature = req.query.signature;
   var timestamp = req.query.timestamp;
   var nonce = req.query.nonce;
+  var echostr=req.query.echostr;
 
-  var fromUsername = req.query.fromUsername;
-  var ToUserName = req.query.ToUserName;
-  var Content = req.query.Content;
-  var time = new Date().getTime();
 
   console.log("signature:"+signature);
   console.log("timestamp:"+timestamp);
   console.log("nonce:"+nonce);
 
-  console.log("fromUsername:"+fromUsername);
-  console.log("ToUserName:"+ToUserName);
-  console.log("Content:"+Content);
-  console.log("time:"+time);
+  res.write(echostr);
 
-  var body = 'Hello World';
-  res.setHeader('Content-Type', 'text/plain');
-
-
-  var textTpl = "<xml>"+
-    "  <ToUserName><![CDATA["+ToUserName+"]]></ToUserName>"+
-  "  <FromUserName><![CDATA["+fromUsername+"]]></FromUserName>"+
-  "  <CreateTime>"+time+"</CreateTime>"+
-  " <MsgType><![CDATA[text]]></MsgType>"+
-  " <Content><![CDATA[Welcome to wechat world!]]></Content>"+
-  "<FuncFlag>0</FuncFlag>"+
-  "</xml>";
-
-  res.setHeader('Content-Length', Buffer.byteLength(textTpl));
-  console.log(textTpl);
-  res.end(textTpl);
-
-
-
-
+  res.end();
 
 });
 
